7aac403fd22a1f084c19a226f6d4a2539b9be07a
Mark Rodonis <mrodonis@gmail.com>
2013-06-26 15:40:48 -0400

    Fixing _Maincontent

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
   def index
-
+    @featured_items = TRUE
+    @meta_title = "Quality Authorized Training"
+    @entry_title = "<b>TRAINING ESSENTIALS</b> | DISCOUNT QUALITY AUTHORIZED TRAINING"
   end
 
   def microsoft
-    @course_list = Course.find_all_by_category 'Microsoft'
+    @tech_ids = Technology.find_all_by_category("Microsoft")
+    # @course_list = Course.find_all_by_technology_id
     @logo_img = 'microsoft-formatted.jpg'
     @logo_size = '672x156'
-
-
+    @meta_title = "Quality Authorized Microsoft Training"
+    @entry_title = "<b>Training Essentials</b> | Authorized Microsoft Training"
   end
 
   def cisco
